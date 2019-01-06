656f2c1f6c8937da330b58007962d8afd74be058
guchaolong <guclno1@qq.com>
2019-01-07 02:39:02 +0800

    design pattern v1

---
+++ src/main/java/com/example/demo/creational_pattern/no3/abstract_factory/OracleStatement.java
@@ -0,0 +1,24 @@
+/**
+ * <p>
+ * For more information about , welcome to http://www.guchaolong.com
+ * <p>
+ * project: design-pattern
+ * <p>
+ * Revision History:
+ * Date          Version       Name            Description
+ * 2019/1/7 1.0          guchaolong          Creation File
+ */
+package com.example.demo.creational_pattern.no3.abstract_factory;
+
+/**
+ * Description:
+ *
+ * @author guchaolong
+ * @date 2019/1/7 1:22
+ */
+public class OracleStatement implements Statement {
+    @Override
+    public void statement() {
+        System.out.println("Oracle statement");
+    }
+}

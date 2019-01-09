7cba0f5d988a4cefbd64e9f4255fbd9cc5ddfbdc
guchaolong <guclno1@qq.com>
2019-01-10 01:37:09 +0800

    bridge pattern

---
+++ src/main/java/com/gcl/designpattern/structural_pattern/no2_bridge/LenovoBrand.java
@@ -0,0 +1,24 @@
+/**
+ * <p>
+ * For more information about , welcome to http://www.guchaolong.com
+ * <p>
+ * project: design-pattern
+ * <p>
+ * Revision History:
+ * Date          Version       Name            Description
+ * 2019/1/10 1.0          guchaolong          Creation File
+ */
+package com.gcl.designpattern.structural_pattern.no2_bridge;
+
+/**
+ * Description:
+ *
+ * @author guchaolong
+ * @date 2019/1/10 1:19
+ */
+public class LenovoBrand implements Brand {
+    @Override
+    public void info() {
+        System.out.println("联想品牌");
+    }
+}

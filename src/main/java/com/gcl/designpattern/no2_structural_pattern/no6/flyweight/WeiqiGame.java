0d5972085f53ed85908800bc6d33c6cedd3ce698
guchaolong <guclno1@qq.com>
2019-06-30 09:50:37 +0800

    fly weight pattern

---
+++ src/main/java/com/gcl/designpattern/no2_structural_pattern/no6/flyweight/WeiqiGame.java
@@ -0,0 +1,23 @@
+/**
+ * <p>
+ * For more information about , welcome to http://www.guchaolong.com
+ * <p>
+ * project: design-pattern
+ * <p>
+ * Revision History:
+ * Date          Version       Name            Description
+ * 2019/6/30 1.0          guchaolong          Creation File
+ */
+package com.gcl.designpattern.no2_structural_pattern.no6.flyweight;
+
+/**
+ * Description:围棋游戏
+ *
+ * @author guchaolong
+ * @date 2019/6/30 9:14
+ */
+public class WeiqiGame {
+    public static void main(String[] args) {
+        new ChessBoard();
+    }
+}

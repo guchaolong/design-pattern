c66e7013aa241b49a96e4c8643cfe470f78c98ff
guchaolong <34919978+guchaolong@users.noreply.github.com>
2023-04-21 23:48:02 +0800

    desgnpattern

--- src/main/java/com/gcl/designpattern/no3_behavioral_pattern/no10_template_method/Client.java
+++ src/main/java/com/gcl/designpattern/no3_behavioral_pattern/no10_template_method/Client.java
@@ -1,21 +1,1 @@
-/**
- * <p>
- * For more information about , welcome to http://www.guchaolong.com
- * <p>
- * project: design-pattern
- * <p>
- * Revision History:
- * Date          Version       Name            Description
- * 2020/4/18 1.0          guchaolong          Creation File
- */
-package com.gcl.designpattern.no3_behavioral_pattern.no10_template_method;
-
-/**
- *
- */
-public class Client {
-    public static void main(String[] args) {
-        BaseHookDemo hookDemo = new SubHookDemo();
-        hookDemo.process();
-    }
-}
+/** * <p> * For more information about , welcome to http://www.guchaolong.com * <p> * project: design-pattern * <p> * Revision History: * Date          Version       Name            Description * 2020/4/18 1.0          guchaolong          Creation File */package com.gcl.designpattern.no3_behavioral_pattern.no10_template_method;/** * */public class Client {    public static void main(String[] args) {        BaseHookDemo hookDemo = new SubHookDemo();        hookDemo.process();    }}
